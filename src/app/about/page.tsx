c94cb52d7105b3e9f9956305a92e22a94ec0704b
s-o-m-i <skullhunter030@gmail.com>
2025-05-10 14:31:56 +0500

    removed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,22 +1,12 @@
-"use client"
 import React from 'react';
 import AboutHero from '@/components/about/AboutHero';
-import OurTeam from '@/components/home/OurTeam';
-import CoreValues from '@/components/about/CoreValues';
-import OurJourney from '@/components/about/OurJourney';
-import AboutServices from '@/components/about/AboutServices';
 import WayAreWeUnique from '@/components/about/WayAreWeUnique';
 import OurStory from '@/components/about/OurStory';
 // import TestimonialsSlider from '../../components/about/TestimonialsSlider';
 // import BelieveSection from '@/components/about/BelieveSection';
 import CallToAction from '@/components/home/CallToAction';
-import dynamic from 'next/dynamic';
-const TestimonialsSlider = dynamic(() => import('../../components/about/TestimonialsSlider'), {
-  ssr: false,
-});
-const BelieveSection = dynamic(() => import('@/components/about/BelieveSection'), {
-  ssr: false,
-});
+import Hello from '@/components/about/Hello';
+
 export const metadata = {
   title: 'About Us | TechNexus',
   description: 'Learn about TechNexus - our story, mission, values and the team behind our innovative IT solutions.',
@@ -27,10 +17,9 @@ export default function AboutPage() {
     <div className="min-h-screen">
       <AboutHero />
       <OurStory/>
-      <BelieveSection/>
-      <AboutServices/>
-      <CoreValues />
-      <TestimonialsSlider/>
+      <Hello/>
+  
+      
       <WayAreWeUnique />
       <CallToAction/>
       {/* <OurJourney /> */}
